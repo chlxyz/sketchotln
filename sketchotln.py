5e9cb4f451f7f306ee86a0e903047d4b1f0e49ab
chlxyz <chloechloechronicleuwu@gmail.com>
2024-01-06 07:46:55 +0700

    fix

--- sketchotln.py
+++ sketchotln.py
@@ -5,9 +5,9 @@ set_output = "" # Specify the output file
 
 def sketchotln(frame, sigma_s=60, sigma_r=0.07):
     # Apply stylization filter for an sketch outline effect
-    oil_painting = cv2.stylization(frame, sigma_s=sigma_s, sigma_r=sigma_r)
+    outlined = cv2.stylization(frame, sigma_s=sigma_s, sigma_r=sigma_r)
 
-    return oil_painting
+    return outlined
 
 def process_video(input_video_path, output_video_path):
     cap = cv2.VideoCapture(input_video_path)
@@ -23,13 +23,13 @@ def process_video(input_video_path, output_video_path):
             break
 
         # Apply the sketch outline effect
-        painted_frame = sketchotln(frame)
+        applied_frame = sketchotln(frame)
 
         # Write the frame to the output video
-        out.write(painted_frame)
+        out.write(applied_frame)
 
         # Display the result (optional)
-        cv2.imshow("Result", painted_frame)
+        cv2.imshow("Result", applied_frame)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
